e89e0bde93444e36063027d905cf6f8ad92860e2
odnodn <odn@gmx.de>
2016-05-10 18:18:23 +0200

    Update promises.js

--- snippets/jaydata/promises.js
+++ snippets/jaydata/promises.js
@@ -30,3 +30,39 @@
                 }
             });
     });
+
+// Chaining async operations
+
+// The following code gets the first two categories (based on default ordering) and then gets their respective products.
+
+    $data.service('/Northwind.svc', function (contextFactory, contextType) {
+        var context = contextFactory();
+        context
+            .Categories
+            .map("it.Category_ID")
+            .take(2)
+            .toArray()
+            .then(function (categoryIds) {
+                console.log("categories received:", categoryIds);
+                return context
+                        .Products
+                        .filter("it.Category_ID in this.ids", { ids: categoryIds })
+                        .toArray();
+            })
+            .then(function (products) {
+                console.log("Products received");
+                console.log(products);
+            });
+    });
+    
+// Waiting for the completion of two or more async operations
+
+// The following code gets the list of all supliers while creating a new product line in the database. We’ll get notified when all of these happened.
+    $data.service('/Northwind.svc', function (contextFactory, contextType) {
+        var context = contextFactory();
+        context.Products.add({ Product_Name: 'My JavaScript Product' });
+        Q.all([context.Suppliers.toArray(), context.saveChanges()])
+         .then(function () {
+             console.log("Multiple operations completed:", arguments);
+         });
+    });    
